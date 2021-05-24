e4fb8d7e68bdd02241271b3e9feab1b73bddabdf
hgrampa <hgrampa21@gmail.com>
2021-05-25 00:06:41 +0300

    Init commit

---
+++ ex00/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+double sum; // Сумма кредита, р
+double rate; // Годовая процентная ставка, %
+double ratePerMonth;  // Процентная ставка по займу в месяц
+int term; // Количество месяцев кредита
+int selectedMonth; // Номер месяца кредита, в котором вносится досрочный платёж
+double payment; // Сумма досрочного платежа, р
+
+if (args.Length == 5
+	&& Double.TryParse(args[0], out sum)
+	&& Double.TryParse(args[1], out rate)
+	&& Int32.TryParse(args[2], out term)
+	&& Int32.TryParse(args[3], out selectedMonth)
+	&& Double.TryParse(args[4], out payment)
+	&& ValidateArgs())
+{
+	ratePerMonth = rate / 12.0f / 100.0f;
+	DateTime firstPaymentDate = GetFirstPaymentDate(); // дата первого платежа (1 число след. месяца)
+	double debtSumm = sum; // Остаток долга
+	double overpaidSumm = 0; // Сумма переплат (до Decrease mounth)
+
+	// Выплачиваем месяцы до Decrease mounth
+	DateTime mouth = firstPaymentDate;
+	double monthlyPaySumm = GetMonthlyPaySumm(sum, term); // Сумма ежемесячного платежа
+	for (int i = 1; i <= selectedMonth; i++)
+	{
+		ApplyMonthlyPayment(ref overpaidSumm, ref debtSumm, mouth, monthlyPaySumm);
+		mouth = mouth.AddMonths(1);
+	}
+	
+
+	double summDecreaseOverpaid = GetSummDecreaseOverpaid(overpaidSumm, debtSumm, mouth); // Сумма переплат при погашении суммы
+	double periodDecreaseOverpaid = GetPeriodDecreaseOverpaid(overpaidSumm, debtSumm, mouth, monthlyPaySumm); // Сумма переплат при погашении строка
+	PrintBetterOverpaid(summDecreaseOverpaid, periodDecreaseOverpaid);
+}
+else
+	Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+
+void PrintBetterOverpaid(double summDecreaseOverpaid, double periodDecreaseOverpaid)
+{
+	
+	if (summDecreaseOverpaid > periodDecreaseOverpaid)
+		Console.WriteLine($"Уменьшение срока выгоднее уменьшения платежа на {summDecreaseOverpaid - periodDecreaseOverpaid:f2} р.");
+	else if (summDecreaseOverpaid < periodDecreaseOverpaid)
+		Console.WriteLine($"Уменьшение платежа выгоднее уменьшения срока на {periodDecreaseOverpaid - summDecreaseOverpaid:f2} р.");
+	else
+		Console.WriteLine("Переплата одинакова в обоих вариантах.");
+}
+
+double GetPeriodDecreaseOverpaid(double overpaidSumm, double debtSumm, DateTime month, double monthlyPaySumm)
+{
+	debtSumm -= payment;
+	int newTerm = (int)Math.Log(monthlyPaySumm / (monthlyPaySumm - ratePerMonth * debtSumm), ratePerMonth + 1);
+	for (int i = 0; i < newTerm; i++)
+	{
+		ApplyMonthlyPayment(ref overpaidSumm, ref debtSumm, month, monthlyPaySumm);
+		month = month.AddMonths(1);
+	}
+	return (overpaidSumm);
+}
+
+double GetSummDecreaseOverpaid(double overpaidSumm, double debtSumm, DateTime month)
+{
+	debtSumm -= payment;
+	double monthlyPaySumm = GetMonthlyPaySumm(debtSumm, term - selectedMonth);
+	for (int i = selectedMonth + 1; i <= term; i++)
+	{
+		ApplyMonthlyPayment(ref overpaidSumm, ref debtSumm, month, monthlyPaySumm);
+		month = month.AddMonths(1);
+	}
+	return (overpaidSumm);
+}
+
+bool ValidateArgs()
+{
+	if (!(Double.IsNormal(sum) && Double.IsNormal(rate) && Double.IsNormal(payment)))
+		return (false);
+	if (sum < 0 || rate < 0 || payment < 0)
+		return (false);
+	if (selectedMonth < 1 && selectedMonth < term)
+		return (false);
+	return (true);
+}
+
+DateTime GetFirstPaymentDate()
+{
+	DateTime now = DateTime.Now.AddMonths(1);
+	return (new DateTime(now.Year, now.Month, 1));
+}
+
+double GetMonthlyPaySumm(double sum, int term)
+{
+	double factor = Math.Pow((1 + ratePerMonth), (double)term); // (1 + i)^n
+	return (sum * ratePerMonth * factor / (factor - 1));
+}
+
+void ApplyMonthlyPayment(ref double overpaidSumm, ref double debtSumm, DateTime month, double monthlyPaySumm)
+{
+	double interestSum = GetInterestInMonth(debtSumm, month);
+	overpaidSumm += interestSum;
+	debtSumm -= (monthlyPaySumm - interestSum);
+}
+
+int DaysInYear(DateTime date)
+{
+	return DateTime.IsLeapYear(date.Year) ? 366 : 365;
+}
+
+double GetInterestInMonth(double debtSumm, DateTime month)
+{
+	DateTime lastmonth = month.AddMonths(-1);
+
+	return (debtSumm * rate * (DateTime.DaysInMonth(lastmonth.Year, lastmonth.Month))) / (100 * DaysInYear(lastmonth));
+}
---
+++ ex01/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+
+
+string[] nameList = LoadNameList();
+
+Console.WriteLine(">Enter name:");
+string name = Console.ReadLine().ToLower();
+if (!ValidateName(name))
+	return;
+if (IsNameInList(name, nameList))
+	Console.WriteLine($"Hello, {NameRepresentation(name)}!");
+else
+{
+	string option;
+	string answer;
+
+	for (int i = 0; i < nameList.Length; i++)
+	{
+		option = nameList[i];
+		if (DistanceLevenshtein(option, name) <= 2)
+		{
+			while (true)
+			{
+				Console.WriteLine($">Did you mean \"{NameRepresentation(option)}\"? Y/N");
+				answer = Console.ReadLine().ToUpper();
+				if (answer == "Y")
+				{
+					Console.WriteLine($"Hello, {NameRepresentation(option)}!");
+					return;
+				}
+				else if (answer == "N")
+					break;
+			}
+		}
+	}
+	Console.WriteLine("Name not found.");
+}
+
+static bool ValidateName(string name)
+{
+	if (name == null)
+		return (false);
+	else if (name.Length == 0)
+		return (false);
+	else if (name.ToUpper() == "X AE A-12")
+	{
+		Console.WriteLine("There was a quick, unscheduled disassembly of the prototype.");
+		return (false);
+	}
+	// TODO regx
+	return (true);
+}
+
+static string NameRepresentation(string name)
+{
+	return (String.Concat(name[0].ToString().ToUpper(), name.Substring(1)));
+}
+
+static string[] LoadNameList()
+{
+	string[] nameList = File.ReadAllLines("us.txt");
+	for (int i = 0; i < nameList.Length; i++)
+	{
+		nameList[i] = nameList[i].ToLower();
+	}
+	return (nameList);
+}
+
+static int GetLevenshteinDistance(string name, string option)
+{
+	int n = name.Length;
+	int m = option.Length;
+	int[,] d = new int[n + 1, m + 1];
+
+	// Step 1
+	if (n == 0)
+	{
+		return m;
+	}
+
+	if (m == 0)
+	{
+		return n;
+	}
+
+	// Step 2
+	for (int i = 0; i <= n; d[i, 0] = i++)
+	{
+	}
+
+	for (int j = 0; j <= m; d[0, j] = j++)
+	{
+	}
+
+	// Step 3
+	for (int i = 1; i <= n; i++)
+	{
+		//Step 4
+		for (int j = 1; j <= m; j++)
+		{
+			// Step 5
+			int cost = (option[j - 1] == name[i - 1]) ? 0 : 1;
+
+			// Step 6
+			d[i, j] = Math.Min(
+				Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+				d[i - 1, j - 1] + cost);
+		}
+	}
+	// Step 7
+	return d[n, m];
+}
+
+static int MinAmongFreeNumbers(int x, int y, int z)
+{
+	if (x > y)
+		x = y;
+	if (x < z)
+		return (x);
+	return (z);
+}
+
+static int DistanceLevenshtein(string namecons, string namedict)
+{
+	var n = namecons.Length + 1;
+	var m = namedict.Length + 1;
+	var matrixDistLev = new int[n, m];
+
+	for (var i = 0; i < n; i++)
+		matrixDistLev[i, 0] = i;
+
+	for (var j = 0; j < m; j++)
+		matrixDistLev[0, j] = j;
+
+	for (var i = 1; i < n; i++)
+	{
+		for (var j = 1; j < m; j++)
+		{
+			var changeel = 0;
+			if (namecons[i - 1] == namedict[j - 1])
+				changeel = 0;
+			else
+				changeel = 1;
+
+			matrixDistLev[i, j] = MinAmongFreeNumbers(matrixDistLev[i - 1, j] + 1,
+				matrixDistLev[i, j - 1] + 1,
+				matrixDistLev[i - 1, j - 1] + changeel);
+		}
+	}
+	return matrixDistLev[n - 1, m - 1];
+}
+
+static bool IsNameInList(string name, string[] nameList)
+{
+	foreach (string line in nameList)
+	{
+		if (name == line)
+			return (true);
+	}
+	return (false);
+}
